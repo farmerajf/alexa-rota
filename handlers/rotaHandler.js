fe1343008683a0910fa563ba72507acb7cc02143
Adam Farmer <adam@mradam.co.uk>
2018-01-12 15:48:43 +0000

    Updated to the latest version

--- handlers/rotaHandler.js
+++ handlers/rotaHandler.js
@@ -1,34 +1,124 @@
-var model = {
-    nextPersonId: "adam-farmer",
-    people: [
-        {
-            id: "adam-farmer",
-            nickname: "Adam",
-            purchases: [
+function log(message) {
+    console.log(message);
+}
 
-            ]
-        },
-        {
-            id: "ashley-nolan",
-            nickname: "Ashley",
-            purchases: [
+function respond(context, response) {
+    log("Response: " + response);
+    context.emit(":tell", response);
+}
 
+function getModel(context) { 
+    log("Getting model")
+    var model = context.attributes['model'];
+    log("Got model: " + JSON.stringify(model));
+    if (model === undefined) {
+        log("Model was empty so setting to deafult");
+        model = {
+            nextPersonIndex: 0,
+            people: [
             ]
-        }
-    ]
-};
+        };
+    }
+
+    log("Using model: " + JSON.stringify(model));
+    return model;
+}
+
+function saveModel(context, model) {
+    log("Saving model: " + JSON.stringify(model));
+    context.attributes['model'] = model;
+}
 
 function getNextPersonName(model) {
-    var nextPerson = model.people.filter((x) => x.id == model.nextPersonId)[0];
-    console.log(nextPerson);
-    return nextPerson.nickname;
+    log("Getting next person");
+    var nextPerson = model.people[model.nextPersonIndex];
+    log("Next person is: " + nextPerson);
+    return nextPerson;
+}
+
+function updateNextPerson(model) {
+    log("Updating next person");
+    var currentPersonIndex = model.people.findIndex((x) => x.id == model.nextPersonIndex);
+    log("Current person index is: " + model.nextPersonIndex);
+    var nextPersonIndex = model.nextPersonIndex + 1;
+    if (nextPersonIndex >= model.people.length) {
+        nextPersonIndex = 0;
+    }
+    log("Next person index is: " + nextPersonIndex);
+
+    model.nextPersonIndex = nextPersonIndex;
 }
 
 module.exports = {
+    'Unhandled': function() {
+        this.emit(':tell', 'This is an unhandled intent');
+    },
+
     'NextPersonIntent': function() {
-        this.emit(":tell", "<audio src='https://s3.amazonaws.com/cookie-monster-sounds/Hom-converted.mp3' /> Cookie monster said, the next person to buy cookies is " + getNextPersonName(model));
-        //this.response.speak("Cookie monster said, the next person to buy cookies is " + getNextPersonName(model))
-        //.audioPlayerPlay("REPLACE_ALL", "https://s3.amazonaws.com/cookie-monster-sounds/Hom+nom+nom+nom+nom.mp3","blah");
-        //this.emit(":responseReady");
+        log("Next person intent");
+        model = getModel(this);
+        if (model.people.length == 0) {
+            respond(this, "There isn't anyone on the cookie rota yet");
+        } else {
+            respond(this, "The next person to buy cookies is " + getNextPersonName(model));
+        }
+    },
+
+    'AddPersonIntent': function() {
+        log("Add person intent");
+
+        var name = this.event.request.intent.slots.name.value;
+        log ("Name to add is: " + name);
+
+        var model = getModel(this);
+        model.people.push(name);
+        log("Updated model is: " + JSON.stringify(model));
+
+        saveModel(this, model);
+        respond(this, "Ok, I've added " + name + " to the cookie rota");
+    },
+
+    'ListPeopleIntent': function() {
+        //TODO: Cope with no people
+        log("List people intent");
+
+        var model = getModel(this);
+        var numberOfPeople = model.people.length;
+        var people = model.people;
+
+        var listOfPeople = people[0];
+        var peopleCountResponse = "";
+        if (numberOfPeople > 2) {
+            for (i = 1; i < numberOfPeople - 1; i++) {
+                listOfPeople = listOfPeople + ", " + model.people[i];
+            }
+        }
+        if (numberOfPeople > 1) {
+            listOfPeople = listOfPeople + " and " + people[numberOfPeople - 1];
+            peopleCountResponse = "There are " + numberOfPeople + " people. ";
+        } else {
+            peopleCountResponse = "There is " + numberOfPeople + " person. ";
+        }
+
+        log("List of people is: " + listOfPeople);
+        respond(this, peopleCountResponse + listOfPeople);
+    },
+
+    'MoveNextIntent': function() {
+        //TODO: Cope with no people on rota
+
+        var model = getModel(this);
+        updateNextPerson(model);
+        saveModel(this, model);
+        respond(this, "Ok. Now the next person to buy cookies is " + getNextPersonName(model));
+    },
+
+    'ResetIntent': function() {
+        var model = {
+            nextPersonIndex: 0,
+            people: ["Adam", "Ash", "Nick", "Jon", "Alastair"]
+        };
+        saveModel(this, model);
+        respond(this, "Ok, I've reset the rota.");
     }
 }
--- main.js
+++ main.js
@@ -2,7 +2,8 @@
 const alexaSdk = require('alexa-sdk');
 
 module.exports.handleRequest = (event, context, callback) => {
-  var alexa = alexaSdk.handler(event, context);
+	var alexa = alexaSdk.handler(event, context);
+	alexa.dynamoDBTableName = 'cookie-rota';
 	alexa.registerHandlers(
 		require('./handlers/rotaHandler.js')
 	);
